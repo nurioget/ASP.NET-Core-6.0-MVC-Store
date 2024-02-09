10194a83414ce4831b9dfbfa82eb39ad30ae170c
nurioget <nurioget9@gmail.com>
2024-02-09 12:01:59 +0300

    ViewComponent

---
+++ Store/Components/CategoriesMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Services.Contracts;
+
+namespace Store.Components
+{
+    public class CategoriesMenuViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _managar;
+
+        public CategoriesMenuViewComponent(IServiceManager managar)
+        {
+            _managar = managar;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _managar.CategoryService.GetAllCategory(false);
+            return View(categories);
+        }
+    }
+}
---
+++ Store/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Services.Contracts;
+
+namespace Store.Components 
+{
+    public class ProductSummaryViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public ProductSummaryViewComponent(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public string Invoke()
+        {
+            return _manager.ProductService.GetAllProcucts(false).Count().ToString();
+        }
+    }
+}
